afc2c4f92a551c0fb5034cc2154e7d3f38f9cd2b
VixonyAn <vixonyan@gmail.com>
2025-11-25 13:24:59 +0100

    change to object filename and note added

--- LysSensorLib/LogEntry.cs
+++ LysSensorLib/LogEntry.cs
@@ -2,6 +2,9 @@
 {
     public class LogEntry
     {
-
+		/*Data to database:
+        What Time the light is turned on
+        What the Light Level is outside
+        Whether the curtain was opened*/
     }
 }
