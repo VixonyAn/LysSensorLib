ef6c558304f49220a34f8760e2e71acb0641e95f
Sofie Kobbernagel Jensen <sofie.jensen.03@gmail.com>
2025-11-25 13:29:25 +0100

    LogEntry

--- LysSensorLib/LogEntry.cs
+++ LysSensorLib/LogEntry.cs
@@ -2,9 +2,27 @@
 {
     public class LogEntry
     {
-		/*Data to database:
+        /*Data to database:
         What Time the light is turned on
         What the Light Level is outside
         Whether the curtain was opened*/
+
+        public DateTime TimeTurnedOn { get; set; }
+        public int OutsideLightLevel { get; set; } //in Lux
+        public bool CurtainOpened { get; set; }
+
+        //Constructor for LogEntry
+        public LogEntry(int outsideLightLevel, bool curtainsOpened)
+        {
+            TimeTurnedOn = DateTime.Now;
+            OutsideLightLevel = outsideLightLevel;
+            CurtainOpened = curtainsOpened;
+        }
+
+        //Default constructor for LogEntry
+        public LogEntry()
+        {
+        
+        }
     }
 }
