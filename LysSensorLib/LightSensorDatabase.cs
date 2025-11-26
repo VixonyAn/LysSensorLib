34ed1115fb98d65246cf542a90eb6debfa29a569
Sofie Kobbernagel Jensen <sofie.jensen.03@gmail.com>
2025-11-26 13:33:49 +0100

    Setup

---
+++ LysSensorLib/LightSensorDatabase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LysSensorLib
+{
+    public class LightSensorDatabase
+    {
+        //private string connectionString = ;
+
+        public LogEntry Add(LogEntry l)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LogEntry? Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<LogEntry> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LogEntry? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LogEntry? Update(int id, LogEntry data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
