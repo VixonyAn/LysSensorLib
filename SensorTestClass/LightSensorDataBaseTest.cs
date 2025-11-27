fc0a8db878c2db4b047789c4da1b9579a9462e8a
Kristoffer Aron Hendriksson <krh004@edu.zealand.dk>
2025-11-27 13:41:06 +0100

    LogEntry ID property

--- SensorTestClass/LightSensorDataBaseTest.cs
+++ SensorTestClass/LightSensorDataBaseTest.cs
@@ -20,7 +20,7 @@ namespace SensorTestClass
             LogEntry logEntry = new LogEntry(500, true, false);
             //act
             _lightSensorDatabase.Add(logEntry);
-            var AllData = _lightSensorDatabase.GetAll();
+            var AllData = _lightSensorDatabase.Get();
             var LastEntry = AllData.Last();
             //assert 
             Assert.AreEqual(logEntry, LastEntry);
@@ -29,7 +29,7 @@ namespace SensorTestClass
         public void GetAllTest()
         {
             //act 
-            var AllData = _lightSensorDatabase.GetAll();
+            var AllData = _lightSensorDatabase.Get();
             //Assert
             Assert.IsNotNull(AllData);
         }
@@ -37,7 +37,7 @@ namespace SensorTestClass
         public void GetByIdTest()
         {
             //act 
-            var AllData = _lightSensorDatabase.GetAll();
+            var AllData = _lightSensorDatabase.Get();
             var LastEntry = AllData.Last();
             var RetrievedEntry = _lightSensorDatabase.GetById(LastEntry.Id);
             //Assert
@@ -48,7 +48,7 @@ namespace SensorTestClass
         public void DeleteObjectTest()
         {
             //act 
-            var AllData = _lightSensorDatabase.GetAll();
+            var AllData = _lightSensorDatabase.Get();
             var LastEntry = AllData.Last();
             _lightSensorDatabase.Delete(LastEntry.Id);
             var DeletedEntry = _lightSensorDatabase.GetById(LastEntry.Id);
