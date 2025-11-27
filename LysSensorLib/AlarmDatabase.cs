87da65be766cc513be38efce61b7b818d55e4d86
Sofie Kobbernagel Jensen <sofie.jensen.03@gmail.com>
2025-11-27 11:52:04 +0100

    alarm

---
+++ LysSensorLib/AlarmDatabase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LysSensorLib
+{
+    public class AlarmDatabase
+    {
+        //private string connectionString = ;
+
+        public LogEntry Add(Alarm a)
+        {
+            throw new NotImplementedException();
+            //husk tilføj logentry når alarmen går af
+        }
+
+        public LogEntry? Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<LogEntry> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LogEntry? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LogEntry? Update(int id, LogEntry data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
