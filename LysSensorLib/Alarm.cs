76737f17a36e555e6c3a5d4ad65674bf840742b2
Sofie Kobbernagel Jensen <sofie.jensen.03@gmail.com>
2025-11-27 11:45:23 +0100

    alarm

---
+++ LysSensorLib/Alarm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LysSensorLib
+{
+    public class Alarm
+    {
+        public string Day { get; set; }
+        public TimeOnly Time { get; set; }
+
+        public Alarm(string day, TimeOnly time)
+        {
+            Day = day;
+            Time = time;
+        }
+    }
+}
