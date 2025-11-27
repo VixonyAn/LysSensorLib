c4f5d60b99a4f1a0425b023675ddf1b53ac59208
Wess L. Toft <weslyn.l.toft@gmail.com>
2025-11-27 10:44:40 +0100

    Unit test done, Missing "Should fail" tests

---
+++ SensorTestClass/LightSensorDataBaseTest.cs
@@ -0,0 +1,60 @@
+﻿using LysSensorLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorTestClass
+{
+    [TestClass]
+    public sealed class LightSensorDataBaseTest
+    {
+        //Remember "Should fail" Tests!!! c: 
+
+        private readonly LightSensorDatabase _lightSensorDatabase;
+        [TestMethod]
+        public void AddObjectTest()
+        {
+            //arrange 
+            LogEntry logEntry = new LogEntry(500, true, false);
+            //act
+            _lightSensorDatabase.Add(logEntry);
+            var AllData = _lightSensorDatabase.GetAll();
+            var LastEntry = AllData.Last();
+            //assert 
+            Assert.AreEqual(logEntry, LastEntry);
+        }
+        [TestMethod]
+        public void GetAllTest()
+        {
+            //act 
+            var AllData = _lightSensorDatabase.GetAll();
+            //Assert
+            Assert.IsNotNull(AllData);
+        }
+        [TestMethod] 
+        public void GetByIdTest()
+        {
+            //act 
+            var AllData = _lightSensorDatabase.GetAll();
+            var LastEntry = AllData.Last();
+            var RetrievedEntry = _lightSensorDatabase.GetById(LastEntry.Id);
+            //Assert
+            Assert.AreEqual(LastEntry, RetrievedEntry);
+        }
+
+        [TestMethod]
+        public void DeleteObjectTest()
+        {
+            //act 
+            var AllData = _lightSensorDatabase.GetAll();
+            var LastEntry = AllData.Last();
+            _lightSensorDatabase.Delete(LastEntry.Id);
+            var DeletedEntry = _lightSensorDatabase.GetById(LastEntry.Id);
+            //Assert
+            Assert.IsNull(DeletedEntry);
+
+        }
+    }
+}
--- SensorTestClass/LogEntryTest.cs
+++ SensorTestClass/LogEntryTest.cs
@@ -1,11 +1,39 @@
-﻿namespace SensorTestClass
+﻿using LysSensorLib;
+
+namespace SensorTestClass
 {
     [TestClass]
     public sealed class LogEntryTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CreateObjectTest()
         {
+            //arrange 
+            LogEntry logEntry = new LogEntry(500, true, false);
+
+            //assert 
+            Assert.AreEqual(500, logEntry.OutsideLightLevel);
+            Assert.AreEqual(true, logEntry.CurtainOpened);
+            Assert.AreEqual(false, logEntry.LightOn);
+
+        }
+
+        [TestMethod]
+        public void ToStringTest()
+        {
+            //arrange 
+            LogEntry logEntry = new LogEntry(300, false, true);
+            //act
+            var toStringResult = logEntry.ToString();
+            //assert
+            Assert.AreEqual("LogEntry [TimeTurnedOn = " + logEntry.TimeTurnedOn + ", OutsideLightLevel = " + logEntry.OutsideLightLevel
+                    + "lux, CurtainOpened = " + logEntry.CurtainOpened + "]", toStringResult);
+        }
+
+        [TestMethod]
+        public void TestFail() 
+        {
+            throw new NotImplementedException();
         }
     }
 }
