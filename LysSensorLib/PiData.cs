2e581b239cc3f740515d086bede82f06ba73f645
Sofie Kobbernagel Jensen <sofie.jensen.03@gmail.com>
2025-12-08 11:09:58 +0100

    Pidata

---
+++ LysSensorLib/PiData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LysSensorLib
+{
+    public class PiData
+    {
+        public int Id { get; set; }
+        public int LightValue { get; set; }
+
+        public PiData(int lightValue)
+        {
+            LightValue = lightValue;
+        }
+    }
+}
---
+++ LysSensorLib/PiDataRepositoryDB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LysSensorLib
+{
+    public class PiDataRepositoryDB
+    {
+        private readonly PiDataDBContext _context;
+        public PiDataRepositoryDB(PiDataDBContext context)
+        {
+            _context = context;
+        }
+
+        public PiData? Get()
+        {
+            return _context.PiData.LastOrDefault();
+        }
+
+    }
+}
