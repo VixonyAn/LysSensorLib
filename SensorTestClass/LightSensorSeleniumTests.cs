1c726cdecb6a3204a71ac327baec2e598519f8f9
VixonyAn <vixonyan@gmail.com>
2025-12-09 12:42:27 +0100

    Added selenium test class and two tests, ButtonExists, and DateCorrect

---
+++ SensorTestClass/LightSensorSeleniumTests.cs
@@ -0,0 +1,58 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SensorTestClass
+{
+	[TestClass]
+	public sealed class LightSensorSeleniumTests
+	{
+		private static IWebDriver driver;
+
+		[ClassInitialize]
+		public static void Setup(TestContext context)
+		{
+			ChromeOptions options = new ChromeOptions();
+			options.AddArguments("--headless=new");
+			driver = new ChromeDriver();
+			driver.Navigate().GoToUrl("https://lysogliv.azurewebsites.net/Vue/index.html");
+		}
+
+		[ClassCleanup]
+		public static void Cleanup()
+		{
+			driver.Quit();
+		}
+
+
+		[TestMethod]
+		public void TestButtonExists()
+		{
+			//Act
+			IWebElement button = driver.FindElement(By.Id("btnSun"));
+			var btnTag = button.TagName;
+
+			//Assert
+			Assert.AreEqual("button", btnTag);
+		}
+
+		[TestMethod]
+		public void TestDateCorrect()
+		{
+			//Arrange
+			IWebElement btnSun = driver.FindElement(By.Id("btnSun"));
+			btnSun.Click();
+			var dateNow = DateTime.UtcNow.ToString();
+			IWebElement btnBurger = driver.FindElement(By.Id("btnBurger"));
+			btnBurger.Click();
+			IWebElement refData = driver.FindElement(By.Id("refData"));
+			refData.Click();
+
+			//Act			
+			IWebElement dataEntry = driver.FindElement(By.Id("DataDate"));
+			var dataDate = dataEntry.GetAttribute("timeTurnedOn");
+
+			//Assert
+			Assert.AreEqual(dateNow, dataDate);
+		}
+	}
+}
